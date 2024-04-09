3538c7e7d1d62d18feb44303b7e9caa992256f56
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2024-04-09 09:51:30 -0700

    20240409-Update
    ContactPro project start

---
+++ ContactPro/ContactPro/Program.cs
@@ -0,0 +1,47 @@
+using ContactPro.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Connection string points to the database connection.Configure default connection in appsettings.json
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//Instead of connecting directly to appsettings.json , we refer to the secrets.json file , which is not publicly exposed and we obtain the database connection from it 
+var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
