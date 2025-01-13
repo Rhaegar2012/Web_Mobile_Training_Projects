6adeaac80916c8b4068ae9cafbe29b25d8875b25
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2025-01-13 06:50:01 -0800

    20250113-Update

--- JavaScriptRefresher/assets/scripts/app.js
+++ JavaScriptRefresher/assets/scripts/app.js
@@ -1,3 +1,4 @@
+import { read } from "fs";
 import {apiKey} from "./util.js"; //import a single variable from an external file
 import * as util from "./util.js"; // Imports the complete util.js file
 console.log(apiKey); //print to the console , using node command from node.js
@@ -86,3 +87,94 @@ console.log(index);
 //Map function , modifies every item in the collection 
 const editedHobbies=hobbies.map((item)=>item+"!");
 console.log(editedHobbies);
+
+//Destructuring
+//The variables defined within the array declaration map to the values of the array 
+const [firstName,lastName] = ["Max","Scuarzmuller"];
+
+console.log(firstName);
+console.log(lastName);
+
+// Destructuring for objects, the variable names in curly brackets map to the object attributes , the variable names must be equal to the object attributes names
+
+const {name:userName,age}={
+    name:"Max",
+    age:34
+};
+
+console.log(userName);
+console.log(age);
+
+//Spread Operator
+const newHobbies =["Reading"];
+//Spread operator ... merges two arrays into one WITHOUT nesting
+const mergedHobbies =[...hobbies,...newHobbies];
+console.log(mergedHobbies);
+const nestedHobbies =[hobbies,newHobbies];
+console.log(nestedHobbies);
+
+//Spread operator on objects 
+const extendedUser ={
+    isAdmin:true,
+    ...user
+};
+
+console.log(extendedUser);
+
+//Control structures
+//if statement using readline from node js
+(async ()=>{
+const readLine = await import('readline');
+let password
+const rl= readLine.createInterface({
+    input: process.stdin,
+    output:process.stdout
+});
+
+//prompt the user for input 
+
+rl.question('Your password',function(input){
+    password=input;
+    if(password ==="Hello")
+        {
+            console.log("Hello Works");
+        }
+    else if(password ==="hello")
+        {
+            console.log("hello works");
+        }
+    else{
+        console.log("Access not granted");
+    }
+    rl.close();
+})
+})();
+
+///For loops 
+for(const hobby of hobbies){
+    console.log(hobby);
+}
+
+//Functions as values
+// pass anonymous function or create it manually in a separate step 
+function handleTimeout(){
+    console.log("Timed out!");
+}
+
+const handleTimeout2=()=>{
+    console.log("Timed out....again!");
+};
+
+setTimeout(handleTimeout,2000);
+
+setTimeout(()=>{
+    console.log('More timing out...');
+},4000);
+
+//Primitive vs reference values
+let newUserMessage = "Hello!";
+newUserMessage = userMessage.concat("!!!");
+
+hobbies.push("Working");
+console.log(hobbies);
+
