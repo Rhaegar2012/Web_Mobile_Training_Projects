303b6c7d1134167904f285d21de7f5f65d9e8ca3
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2025-01-21 07:49:23 -0800

    20250121-Update

--- ReactFundamentals/src/App.jsx
+++ ReactFundamentals/src/App.jsx
@@ -1,7 +1,9 @@
 
 import {CORE_CONCEPTS} from './data.js';
-import Header from './components/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx';
+import Header          from './components/Header.jsx'
+import CoreConcept     from './components/CoreConcept.jsx';
+import TabButton       from './components/TabButton.jsx';
+
 
 
 
@@ -14,6 +16,11 @@ import CoreConcept from './components/CoreConcept.jsx';
 
 //Default component
 function App() {
+  function handleSelect(selectedButton){
+    // selectedButton => 'components, 'jsx' , 'props' 
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header></Header>
@@ -26,6 +33,15 @@ function App() {
             <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
