a37caaae509fec3bbf44f043766db299b44292dc
Jose Tellez <48417377+Rhaegar2012@users.noreply.github.com>
2025-04-28 08:00:21 -0700

    20250428-Update

--- JavaScript Fundamentals/robot.js
+++ JavaScript Fundamentals/robot.js
@@ -298,3 +298,17 @@ function findRoute(graph,from,to){
         }
     }
 }
+
+//Goal Oriented Robot
+function goalOrientedRobot({place,parcels},route){
+    if(route.length==0){
+        let parcel = parcels[0];
+        if(parcel.place!=place){
+            route = findRoute(roadGraph, place, parcel.place);
+        }else{
+            route=findRoute(roadGraph,place, parcel.address);
+        }
+    }
+    return {direction:route[0],memory:route.slice(1)};
+
+}
